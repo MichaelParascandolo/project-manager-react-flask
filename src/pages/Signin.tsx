12b1fb78a2eb724f3c8ac9e80272266515845491
Michael Parascandolo <michaelparascandolo@me.com>
2023-03-04 12:52:19 -0500

    Signin page UI / UX changes

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,57 +1,52 @@
 import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 import axios from "axios";
 import Footer from "../components/Footer";
 import Logo from "../components/Logo";
 
 function Signin(props: any) {
-  const [wrongPassword, setWrongPassword] = useState<boolean>(false);
-  const [loginForm, setLoginForm] = useState({
-    email: "",
-    password: "",
-  });
-
-  function logMeIn(event: any) {
-    event.preventDefault();
-    if (loginForm.email != "" && loginForm.password != "") {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  function delay(ms = 1000): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+  function logMeIn(e: any) {
+    const toastId = toast.loading("Please wait...");
+    e.preventDefault();
+    if (email != "" && password != "") {
       axios({
         method: "POST",
         url: "http://127.0.0.1:3000/token",
         data: {
-          email: loginForm.email,
-          password: loginForm.password,
+          email: email,
+          password: password,
         },
       })
-        .then((response) => {
+        .then(async (response) => {
+          toast.success("Signin Successful", {
+            id: toastId,
+          });
+          await delay();
           props.setToken(response.data.access_token);
-          setWrongPassword(false);
           // console.log("Email" + loginForm.email);
           // console.log("Password" + loginForm.password);
           // console.log("Token" + response.data.access_token);
         })
-        .catch((error) => {
+        .catch(async (error) => {
           if (error.response) {
-            setWrongPassword(true);
-            console.log(error.response);
-            console.log(error.response.status);
-            console.log(error.response.headers);
+            await delay();
+            toast.error("Invalid Credentials", {
+              id: toastId,
+            });
+            setEmail("");
+            setPassword("");
+            // console.log(error.response);
+            // console.log(error.response.status);
+            // console.log(error.response.headers);
           }
         });
-
-      setLoginForm({
-        email: "",
-        password: "",
-      });
     }
   }
-
-  function handleChange(event: any) {
-    const { value, name } = event.target;
-    setLoginForm((prevNote) => ({
-      ...prevNote,
-      [name]: value,
-    }));
-  }
-
   const styles = {
     links:
       "text-blue-500 border-b-2 border-transparent hover:border-blue-500 ease-in-out transition-all duration-300",
@@ -61,6 +56,7 @@ function Signin(props: any) {
   };
   return (
     <div className="flex min-h-screen justify-center">
+      <Toaster />
       <div className="my-auto w-full max-w-[550px] p-4">
         <div className="my-auto">
           <div className="-mb-6">
@@ -69,34 +65,27 @@ function Signin(props: any) {
           <div className="mt-10 bg-slate-800 shadow-xl shadow-black rounded-xl border-2 border-slate-900 h-[400px]">
             <div className="flex justify-center">
               <div className="w-[500px] p-8">
-                {!wrongPassword ? (
-                  <h2 className="text-white cap font-bold text-center text-xl tracking-wide">
-                    Sign in to your account
-                  </h2>
-                ) : (
-                  <h2 className="text-red-500 cap font-bold text-center text-xl tracking-wide">
-                    Invalid Credentials
-                  </h2>
-                )}
-
-                <form className="login">
+                <h2 className="text-white cap font-bold text-center text-xl tracking-wide">
+                  Sign in to your account
+                </h2>
+                <form onSubmit={logMeIn}>
                   <p className={styles.label}>Email</p>
                   <input
                     type="email"
-                    name="email"
-                    onChange={handleChange}
+                    value={email}
                     className={styles.input}
                     placeholder="name@company.com"
-                    value={loginForm.email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
                   />
                   <p className={styles.label}>Password</p>
                   <input
                     type="password"
-                    name="password"
-                    onChange={handleChange}
+                    value={password}
                     className={styles.input}
                     placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
-                    value={loginForm.password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
                   />
                   <div className="flex justify-between mt-4">
                     <div className="flex">
@@ -112,8 +101,8 @@ function Signin(props: any) {
                   </div>
                   <div className="flex justify-center">
                     <button
+                      type="submit"
                       className="bg-blue-500 border-2 border-blue-800 text-lg px-4 py-2 mt-4 rounded-lg w-full hover:bg-blue-700 transition-all ease-in-out duration-300"
-                      onClick={logMeIn}
                     >
                       Login to your account
                     </button>
