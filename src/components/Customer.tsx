b30731284aba07d89cfae27f6288fc64a0b90a54
Michael Parascandolo <michaelparascandolo@me.com>
2023-03-09 17:55:24 -0500

    Working on customer component UI

--- src/components/Customer.tsx
+++ src/components/Customer.tsx
@@ -1,4 +1,5 @@
 import { CgProfile, CgMoreVertical, CgPhone, CgMore } from "react-icons/cg";
+import { FaCity } from "react-icons/fa";
 import { useState } from "react";
 
 const Customer = ({ item }: { item: any }) => {
@@ -27,7 +28,10 @@ const Customer = ({ item }: { item: any }) => {
               <p className="tracking-wider">{item.LastName}</p>
             </div>
           </div>
-          {/* <div className="h-full bg-slate-900/50 rounded-xl w-0.5" /> */}
+          <div className="my-auto flex flex-wrap">
+            <FaCity className="text-white" size={30} />
+            <p className="tracking-wider my-auto ml-2">{item.City}</p>
+          </div>
           <div className="my-auto flex flex-wrap">
             <CgPhone className="text-white" size={30} />
             <p className="tracking-wider my-auto ml-2">
@@ -39,13 +43,37 @@ const Customer = ({ item }: { item: any }) => {
         {/* more details */}
         {showDetails ? (
           <>
-            <p className="text-gray-300 text-start py-1">Email: {item.Email}</p>
-            <p className="text-gray-300 text-start py-1">
-              Address: <br />
-              {item.Street}, {item.City}
-              <br />
-              {item.State} {item.ZIP}
-            </p>
+            <div className="flex justify-between w-full">
+              <div>
+                <p className="mt-2 text-start text-lg tracking-wider">
+                  {item.FirstName} {item.LastName}
+                </p>
+                <p className="text-gray-300 text-start py-1">
+                  Email: <br /> {item.Email}
+                </p>
+                <p className="text-gray-300 mt-2 text-start py-1">
+                  Address: <br />
+                  {item.Street}, {item.City}
+                  <br />
+                  {item.State} {item.ZIP}
+                </p>
+              </div>
+              <div className="my-auto flex flex-col">
+                <button
+                  type="submit"
+                  className="bg-blue-500 border-2 text-black border-blue-800 text-lg px-4 py-2 rounded-lg w-[200px] mt-2 hover:bg-blue-700 transition-all ease-in-out duration-300"
+                >
+                  Create Job
+                </button>
+                <button
+                  type="reset"
+                  className="bg-red-500 border-2 text-black border-red-800 text-lg px-4 py-2 rounded-lg mt-2 w-[200px] hover:bg-red-700 transition-all ease-in-out duration-300"
+                >
+                  Delete Customer
+                </button>
+              </div>
+            </div>
+            <div className="bg-slate-900/50 mt-1 rounded-xl h-1 w-full" />
           </>
         ) : null}
         <button
