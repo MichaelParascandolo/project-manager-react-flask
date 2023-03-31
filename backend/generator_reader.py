64e591a581ba3e5294c3f732a64cceb111fddca3
MildlyConcussed <oliver@bruno.bz>
2023-03-31 18:48:05 -0400

    Update generator_reader.py
    Added comments and clarification

--- backend/generator_reader.py
+++ backend/generator_reader.py
@@ -1,13 +1,15 @@
 import csv
+# Example file path: '/Users/oliverbruno/Documents/GitHub/Software-Engineering-Project/backend/testFile.csv'
+file_path = '' # Actual File Path goes here
 
-file = open('/Users/oliverbruno/Documents/GitHub/Software-Engineering-Project/backend/testFile.csv')
+file = open(file_path)
 reader = csv.reader(file)
-header = next(reader)
+header = next(reader) # Pulls the first row of the csv file
 
 rows = []
-for row in reader:
+for row in reader: # Loads every row into a big array full of arrays
     rows.append(row)
 file.close()
 
-print(header)
-print(rows)
+# Rows are in following format:
+# Name, ID, Price, Notes
