277700874e5d6636e1926b8d421a1cdc54e274b5
wailer25 <anobiora@gmail.com>
2023-04-02 12:25:43 -0400

    Made changes to generator reader to add csv files to database WIP

--- backend/generator_reader.py
+++ backend/generator_reader.py
@@ -1,15 +1,24 @@
 import csv
-# Example file path: '/Users/oliverbruno/Documents/GitHub/Software-Engineering-Project/backend/testFile.csv'
-file_path = '' # Actual File Path goes here
+from models import db, Generators
 
-file = open(file_path)
-reader = csv.reader(file)
-header = next(reader) # Pulls the first row of the csv file
 
-rows = []
-for row in reader: # Loads every row into a big array full of arrays
-    rows.append(row)
-file.close()
+def reader():
+    # Example file path: '/Users/oliverbruno/Documents/GitHub/Software-Engineering-Project/backend/testFile.csv'
+    file_path = 'testFile.csv' # Actual File Path goes here
+    file = open(file_path)
+    reader = csv.reader(file)
+    header = next(reader)  # Pulls the first row of the csv file
 
-# Rows are in following format:
-# Name, ID, Price, Notes
+    rows = []
+    for row in reader:
+        if Generators.query.filter_by(GeneratorID = row[1]).first() is None: # Loads every row into a big array full of arrays
+            db.session.add(
+                Generators(Name = row[0], GeneratorID = row[1], Cost = row[2], Notes = row[3])
+            )
+            db.session.commit()
+    file.close()
+    # Rows are in following format:
+    # Name, ID, Price, Notes
+
+if __name__ == "__main__":
+    reader()
