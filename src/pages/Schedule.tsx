a50b12ad8648a4c2792b55b38179c2dda9d8f969
Michael Parascandolo <michaelparascandolo@me.com>
2023-04-05 19:47:57 -0400

    working on schedule

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -2,35 +2,29 @@ import { MdNavigateNext, MdNavigateBefore } from "react-icons/md";
 import Day from "../components/Day";
 import Footer from "../components/Footer";
 
-{/* schedule page showing month week and each individual day*/}
+{
+  /* schedule page showing month week and each individual day*/
+}
 const Schedule = (props: any) => {
   return (
     <>
-      <div className="my-4 flex justify-center">
-        <div className="text-center">
-          <h1 className="text-white text-[30px] mb-2 font-mono tracking-wide">
-            February
-          </h1>
-          <div className="flex justify-evenly">
-            <div className=" bg-slate-400 rounded-full mr-2">
-              <MdNavigateBefore size={25} />
-            </div>
-            <h2 className="text-gray-400 font-bold tracking-wider">
-              6th - 10th
-            </h2>
-            <div className=" bg-slate-400 rounded-full ml-2">
-              <MdNavigateNext size={25} />
-            </div>
-          </div>
+      <div className="flex justify-center mt-5">
+        <div className="w-[500px] p-4 bg-slate-900 border-2 border-slate-800 shadow-md shadow-black mb-2 rounded-xl h-full transition-all duration-300 ease-in-out">
+          <h2 className="text-white cap font-roboto text-center text-4xl tracking-wide">
+            Job Schedule
+          </h2>
+          <h3 className="text-gray-500 uppercase text-sm my-1 text-center tracking-wider">
+            X upcoming jobs
+          </h3>
         </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4 px-2">
+      {/* <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4 px-2">
         <Day day="Monday" date={6} />
         <Day day="Tuesday" date={7} />
         <Day day="Wednesday" date={8} />
         <Day day="Thursday" date={9} />
         <Day day="Friday" date={10} />
-      </div>
+      </div> */}
       <Footer />
     </>
   );
